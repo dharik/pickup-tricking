6e2a6518aa8c6128728aa2bcb2c9734865a6801e
Dharik Patel <dharik92@gmail.com>
2018-10-07 13:13:19 -0700

    slack notifications for new gatherings and users!

---
+++ functions/src/index.ts
@@ -0,0 +1,35 @@
+import * as functions from 'firebase-functions';
+import * as request from 'request-promise';
+
+// // Start writing Firebase Functions
+// // https://firebase.google.com/docs/functions/typescript
+//
+// export const helloWorld = functions.https.onRequest((request, response) => {
+//  response.send("Hello from Firebase!");
+// });
+
+export const newGatheringSlackNotifier = functions.database
+  .ref('/gatherings/{gatheringID}')
+  .onCreate(async snap => {
+    const gatheringStr = snap.toJSON();
+
+    await request({
+      method: 'POST',
+      uri: functions.config().slack.webhook_url,
+      body: {
+        text: 'New gathering created!' + '```' + gatheringStr + '```'
+      },
+      json: true
+    });
+  });
+
+export const newUserSlackNotifier = functions.auth.user().onCreate(async newUser => {
+  await request({
+    method: 'POST',
+    uri: functions.config().slack.webhook_url,
+    body: {
+      text: `New user: ${newUser.displayName}`
+    },
+    json: true
+  });
+});
